54a19821fa46260e6df7e8974cbe4d10fb498c31
ksu <ksuhablondo@gmail.com>
2020-08-21 14:10:42 +0300

    Updates

---
+++ precache-manifest.036656258a07659fe70891a56ee64140.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dd37f67aa919ca93de1ba80c6c8c792",
+    "url": "/postList-testApp/index.html"
+  },
+  {
+    "revision": "11b9d36adab062248439",
+    "url": "/postList-testApp/static/css/main.a919cdb2.chunk.css"
+  },
+  {
+    "revision": "dbc1cdf2479a8a971841",
+    "url": "/postList-testApp/static/js/2.54096cee.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/postList-testApp/static/js/2.54096cee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11b9d36adab062248439",
+    "url": "/postList-testApp/static/js/main.07ebc1d8.chunk.js"
+  },
+  {
+    "revision": "33e7f53879712bffb94b",
+    "url": "/postList-testApp/static/js/runtime-main.1f76ba59.js"
+  },
+  {
+    "revision": "e4d97315e2af206790021c4189eed3fb",
+    "url": "/postList-testApp/static/media/like-active.e4d97315.svg"
+  },
+  {
+    "revision": "d487f4207039e12a220b177bdb39d051",
+    "url": "/postList-testApp/static/media/like.d487f420.svg"
+  }
+]);
